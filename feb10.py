8f36de93d50b160197cf298e014b3129f0a827cd
m-modak <sarahm046@stemcollegiate.ca>
2025-02-10 12:52:24 -0700

    feb10.py

--- feb10.py
+++ feb10.py
@@ -6,3 +6,12 @@ print("ts pmo")
 print("""hello everyone
 its snowing on mount fuji
 i am in japan""")
+print("""M Modak
+February 10th 2025
+""")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes 
+a day. I'll be using Replit, an amazing online IDE so I can do this from my phone 
+wherever I happened to be. No excuses for not coding from the middle of a field!""") 
+print("I am feeling 😊")
+print("You can follow my progress at replit.com/@MModak")
