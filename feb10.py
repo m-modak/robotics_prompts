1dd3ca59098a16d69a921d9f8afd6544930fc046
m-modak <sarahm046@stemcollegiate.ca>
2025-02-10 12:29:29 -0700

    Create feb10.py

---
+++ feb10.py
@@ -0,0 +1,8 @@
+print("hi m")
+print("""I am currently learning how to make 
+line breaks and things with the triple quotation marks
+I could write a haiku wait""")
+print("ts pmo")
+print("""hello everyone
+its snowing on mount fuji
+i am in japan""")
