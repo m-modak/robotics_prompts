38f3d586f071873119feb5c9dbb416c68af84c69
m-modak <sarahm046@stemcollegiate.ca>
2025-02-14 09:35:34 -0700

    feb14.py

---
+++ feb14.py
@@ -0,0 +1,11 @@
+print("let's make a wacky recipe !!")
+print()
+food = input("name a food:")
+plant = input("name a plant:")
+cooking = input("Name a method of cooking:")
+burned = input("name a word that describes bad food:")
+diy = input("name a diy item:")
+
+print("MENU")
+print(cooking, food, "with", burned, plant, "with a side of", diy)
+#intro to concatenating, combining strings and variables in the same sentence. 
