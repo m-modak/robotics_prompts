5ce7408bbe21b2fd5ab4711ac527702aef47f65b
m-modak <sarahm046@stemcollegiate.ca>
2025-03-07 09:41:34 -0700

    Create mar07.py

---
+++ mar07.py
@@ -0,0 +1,11 @@
+username = input("Username > ")
+password = input ("Password > ")
+
+if username == "m" and password == "ace attorney":
+  print("welcome m. i missed you pookielicious kitten whiskers... daddy will talk to you later...")
+elif username == "sofia" and password == "freakydeaky":
+  print("hey sofia... i want you... i've been thinking about you pookiewookie..")
+elif username == "mehreen" and "i don't know...":
+  print("MEHREEN. I NEED YOU. ALPHA IS YEARNING.")
+else:
+  print("GET OUT OF MY SWAMP")
