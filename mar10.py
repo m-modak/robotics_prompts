09fb2342917f049aa6aa2ae5b17f6cd66d5178bb
m-modak <sarahm046@stemcollegiate.ca>
2025-03-10 12:32:25 -0600

    Create mar10.py

---
+++ mar10.py
@@ -0,0 +1,15 @@
+aa = input("are you an ace attorney fan ? yes or no.. ")
+if aa == "yes":
+  print("i have GOT to kiss you on the lips.")
+  char = input("heh.. what's the name of the prosecutor in the second game ?")
+  if char == "franziska von karma":
+    print("HOORAYYY IM SO GLAD YOU KNOW THIS YAYYYY !!!!!!! ")
+  else: 
+    print("sigh...i thought you were a fan..")
+elif aa == "no":
+  print("oh.. okay..")
+  toppings = input("are you perhaps interested in courtoom drama murder mysteries..? ")
+  if toppings == "yes":
+    print("YOU SHOULD PLAY ACE ATTORNEY THEN PLEASEEEE")
+  else:
+    print("get OUT of my sight...")
