74d9b4cd6ca4bc4b44489306be9e9dfa4a8bdbd2
m-modak <sarahm046@stemcollegiate.ca>
2025-04-09 12:34:10 -0600

    Create mar11.py

---
+++ mar11.py
@@ -0,0 +1,32 @@
+print("hello !!! welcome to the daily lucky object generator ")
+name = input("What is your name? ")
+
+
+
+if name =="hannah" or name == "Hannah":
+ DOW = input("what is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("your lucky object a magnetic fork", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("your lucky object is a setting spray", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("your lucky object is a pair of camo boxers", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"your lucky object is a Dyson vacuum")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "your lucky object is a pair of sunglasses")
+
+elif name == "Nick" or name== "nick":
+ DOW = input("what is the day of the week rn?")
+ if DOW == "monday" or DOW == "Monday":
+   print("your lucky object is a brown tree", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("your lucky object is a monstera plant", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("your lucky object is a mechanical pencil", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"your lucky object is an analog clock")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "your lucky object is a can of coke")
+else:
+ print("i do not know your name, but i hope you are having a great day!")
