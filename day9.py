dc95bd98cb50dd3c6831ab31088faf4679af72c8
m-modak <sarahm046@stemcollegiate.ca>
2025-04-10 21:39:13 -0600

    Create day9.py
    I just changed it to the day that it is because i am doing one everyday.. apologies for the inconvenience..

---
+++ day9.py
@@ -0,0 +1,14 @@
+year = int(input("what year were you born in?"))
+if year <= 1925 :
+  print("you're a traditionalist!")
+elif year >= 1947 :
+  print("you're a baby boomer!")
+elif year >= 1965 :
+  print("you're a gen x!")
+elif year >= 1982 :
+  print("you're a millenial!")  
+elif year >= 1996 :
+  print("you're a gen z!")
+else:
+  print("are you. even alive ???")
+  #introduction to inequalities!!
